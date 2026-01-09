84be8743be2c2c10ee628379b4010cbe12a49b11
Thrinadh <thrinadhprasadapu9@gmail.com>
2026-01-10 02:10:50 +0530

    Random Forest with depth

--- src/train.py
+++ src/train.py
@@ -37,8 +37,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 # -----------------------------
 # Model - (Modify per experiment)
 # -----------------------------
-#model = RandomForestRegressor(n_estimators=100, random_state=42,max_depth=10)
-model = LinearRegression()
+model = RandomForestRegressor(n_estimators=100, random_state=42,max_depth=10)
+
 model.fit(X_train, y_train)
 
 # -----------------------------
