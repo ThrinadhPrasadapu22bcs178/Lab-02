6aeb6142e847a9fe1e279e213b4337b32c7d96f2
Thrinadh <thrinadhprasadapu9@gmail.com>
2026-01-10 02:07:01 +0530

    Liner Reg

--- src/train.py
+++ src/train.py
@@ -4,7 +4,8 @@ import joblib
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge,LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 # -----------------------------
@@ -36,7 +37,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 # -----------------------------
 # Model - (Modify per experiment)
 # -----------------------------
-model = Ridge(alpha=1.0)
+#model = RandomForestRegressor(n_estimators=100, random_state=42,max_depth=10)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 # -----------------------------
